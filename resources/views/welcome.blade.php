ddf19c3d2bd1b3060611474145170c0bea1ca410
Nathan Godard <nathan.godard1@ucalgary.ca>
2019-04-07 16:14:42 -0600

    Adding project to github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+@extends('home')
+
+@section('content')
+
+            <div class="content">
+              <div>
+                <h1> Vehicle Details </h1>
+                <h2> Toyota Camry XSE V6 </h2>
+                <p><b>Price: </b>$34,700</p>
+                <img id="exterior" width="450px" src="https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/exterior/1/864_477_PNG/3735f51-225a251-449c20a-9ec3659-457acd3-6cfc754-b01ca0d-3916915-01c4b64-c3b81ed-f3a0a0e-24605c1-825b44a-e1959bb-b020e84-289cdd7-583e260-ba384c7.png" alt="Exterior Image">
+<br />
+                <a href="#" style="text-decoration:none" onclick="document.images['exterior'].src ='https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/exterior/1/864_477_PNG/3735f51-1e8d466-449c20a-149b969-a8c8b37-6cfc754-667638d-7be69a3-01c4b64-7330a26-2067707-24605c1-825b44a-e1959bb-b020e84-289cdd7-583e260-ba384c7.png'">
+                  <img width="20px" height="20px" src="https://www.debscrossstitch.co.uk/ekmps/shops/debscrossstitch/images/bright-red-square-aperture-card-envelope-3.5-x-4.5-2583-p.jpg" alt="red">
+                </a>
+
+                <a href="#" style="text-decoration:none" onclick="document.images['exterior'].src ='https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/exterior/1/864_477_PNG/3735f51-225a251-449c20a-9ec3659-457acd3-6cfc754-b01ca0d-3916915-01c4b64-c3b81ed-f3a0a0e-24605c1-825b44a-e1959bb-b020e84-289cdd7-583e260-ba384c7.png'">
+                  <img width="20px" height="20px" src="https://www.tate.org.uk/art/images/work/T/T02/T02092_10.jpg" alt="black">
+                </a>
+
+                <a href="#" style="text-decoration:none" onclick="document.images['exterior'].src ='https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/exterior/1/864_477_PNG/3735f51-663495b-449c20a-c5d5b46-c98d143-6cfc754-875a2a4-b797f3b-01c4b64-128c5df-fc7fb54-24605c1-825b44a-e1959bb-b020e84-289cdd7-583e260-ba384c7.png'">
+                  <img width="20px" height="20px" src="https://www.colorhexa.com/0367a6.png" alt="blue">
+                </a>
+
+<br />
+<br />
+                <img id="interior" width="300px" src="https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/interior/1/864_477_PNG/43a29f6-bb6ac76-5a294a4-3514495-9507cec-b453fdf-f522f25-f270a3c-49fa29a-b448073-dc604fa-bea809e-1ad852f-439a7f2-184cca1-67757cb-ee54454-72a2d99-7724404-37cdacb-19bb2cf-e36c725.png" alt="Interior Image">
+<br />
+                <a href="#" style="text-decoration:none" onclick="document.images['interior'].src ='https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/interior/1/864_477_PNG/43a29f6-38d96f8-5a294a4-8e64bb1-9507cec-b453fdf-f522f25-f270a3c-49fa29a-b448073-dc604fa-bea809e-1ad852f-439a7f2-184cca1-67757cb-ee54454-72a2d99-7724404-a98c9b5-b4537ae-e36c725.png'">
+                  <img width="20px" height="20px" src="https://www.debscrossstitch.co.uk/ekmps/shops/debscrossstitch/images/bright-red-square-aperture-card-envelope-3.5-x-4.5-2583-p.jpg" alt="red">
+                </a>
+
+                <a href="#" style="text-decoration:none" onclick="document.images['interior'].src ='https://www.toyota.com/config/pub/3d/toyota/1006223/1000854/interior/1/864_477_PNG/43a29f6-bb6ac76-5a294a4-3514495-9507cec-b453fdf-f522f25-f270a3c-49fa29a-b448073-dc604fa-bea809e-1ad852f-439a7f2-184cca1-67757cb-ee54454-72a2d99-7724404-37cdacb-19bb2cf-e36c725.png'">
+                  <img width="20px" height="20px" src="https://www.tate.org.uk/art/images/work/T/T02/T02092_10.jpg" alt="black">
+                </a>
+
+              </div>
+
+            </div>
+            <div style="margin-left: 200px">
+                <h1>Toyota Camry <br />XSE V6</h1>
+                <ul><b>Design:</b>
+                  <li>Aggressive front grille with sport mesh insert, and LED headlights with LED DRL</li>
+                  <li>Dual exhaust with quad chrome tips</li>
+                  <li>19-in. black machined-finish alloy wheels</li>
+                  <li>Gloss-black front grille with sport mesh insert</li>
+                  <li>LED Taillights</li>
+                </ul>
+                <ul><b>Styling:</b>
+                  <li>Panoramic roof with front power tilt/slide moonroof and Cockpit Red leather-trimmed seats</li>
+                  <li>Paddle shifters</li>
+                  <li>Seat detailing</li>
+                </ul>
+                <ul><b>Safety:</b>
+                  <li>Pre-Collision System with Pedestrian Detection</li>
+                  <li>Lane Departure Alert with Steering Assist</li>
+                  <li>Automatic High Beams</li>
+                  <li>Dynamic Radar Cruise Control</li>
+                </ul>
+                <a href="https://www.toyota.com/content/ebrochure/2019/camry_ebrochure.pdf">Download full spec sheet</a>
+            </div>
+
+
+@endsection
